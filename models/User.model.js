5d8fd1b6b2db11d70120a57a2551b41d7edd77d1
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-09-28 21:38:12 +0000

    Start User model

--- models/User.model.js
+++ models/User.model.js
@@ -1,26 +1,11 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: [true, 'Email is required.'],
-      unique: true,
-      lowercase: true,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: [true, 'Password is required.']
-    }
-  },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
-  }
-);
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, unique: true, required },
+  password: { type: String, required: true }
+});
 
-const User = model("User", userSchema);
-
-module.exports = User;
+module.exports = model("User", userSchema);
