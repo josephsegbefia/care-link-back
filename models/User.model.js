71c0348b29b40c481a04d26622486f0d913e5add
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-09-28 23:37:55 +0000

    Modify user model

--- models/User.model.js
+++ models/User.model.js
@@ -1,22 +1,27 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  phone: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function (v) {
-        // Use a regular expression to check for a 10-digit number
-        return /^\d{10}$/.test(v);
-      },
-      message: "Phone number must be a 10-digit number without alphabets"
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    phone: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          // Use a regular expression to check for a 10-digit number
+          return /^\d{10}$/.test(v);
+        },
+        message: "Phone number must be a 10-digit number without alphabets"
+      }
     }
+  },
+  {
+    timestamps: true
   }
-});
+);
 
 module.exports = model("User", userSchema);
