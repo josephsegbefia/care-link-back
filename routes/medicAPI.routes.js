b93b0c0cc70e250b6f496ec2234e67c78a0ddd9d
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-10-10 16:10:49 +0000

    Make complete symptoms route

--- routes/medicAPI.routes.js
+++ routes/medicAPI.routes.js
@@ -4,8 +4,10 @@ const crypto = require("crypto");
 
 const router = express.Router();
 const URI = process.env.URI;
+const API_URI = process.env.API_URI;
 const API_KEY = process.env.API_KEY;
 const SECRET_KEY = process.env.SECRET_KEY;
+let authString = "";
 
 async function makeAuthRequest() {
   const secretBytes = Buffer.from(SECRET_KEY, "utf8");
@@ -15,7 +17,6 @@ async function makeAuthRequest() {
   const dataBytes = Buffer.from(URI, "utf8");
   const computedHash = hmac.update(dataBytes).digest("base64");
   computedHashString = computedHash;
-  const authString = "";
 
   try {
     const response = await axios.post(URI, "", {
@@ -35,10 +36,24 @@ async function makeAuthRequest() {
 router.post("/get-auth", async (req, res, next) => {
   try {
     authString = await makeAuthRequest();
+    console.log(authString);
     res.status(200).json(authString);
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
+router.get("/symptoms", async (req, res, next) => {
+  try {
+    const token = authString.Token;
+    const symptoms = await axios.get(
+      `${API_URI}/symptoms?token=${token}&language=en-gb`
+    );
+
+    res.status(200).json(symptoms.data); // Change the status code to 200 for success
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" }); // Set an appropriate error status code
+  }
+});
 module.exports = router;
