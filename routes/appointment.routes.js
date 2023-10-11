d617ddd40218dd7524a3c1a4102d0a6b8e46db92
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-10-11 14:17:15 +0000

    Finish get appointments

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -51,4 +51,20 @@ router.post("/appointments", async (req, res, next) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
+router.get("/appointments/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const appointment = await Appointment.findOne({
+      $or: [{ _id: id }, { userId: id }, { doctorId: id }]
+    });
+    if (appointment) {
+      res.status(200).json(appointment);
+    } else {
+      res.status(404).json({ message: "Appointment not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 module.exports = router;
